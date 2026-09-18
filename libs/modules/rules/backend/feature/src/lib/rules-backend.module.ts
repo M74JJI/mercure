ab05a3d100685c95712ae8b724d3f26b81935d92
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-18 13:37:37 +0100

    feature:rules:wire-archive-import

--- libs/modules/rules/backend/feature/src/lib/rules-backend.module.ts
+++ libs/modules/rules/backend/feature/src/lib/rules-backend.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 
-import { AnalyzeRuleset, RULESET_ANALYZER } from '@mercure/rules-backend-application';
-import { WazuhXmlRulesetAnalyzer } from '@mercure/rules-backend-infrastructure';
+import { PlatformConfig } from '@mercure/platform-backend-config';
+import {
+  AnalyzeRuleset,
+  ImportArchivedRuleset,
+  RULESET_ANALYZER,
+  RULESET_ARCHIVE_SOURCE,
+  type RulesetArchiveSource,
+} from '@mercure/rules-backend-application';
+import {
+  FilesystemManagerArchiveSource,
+  WazuhXmlRulesetAnalyzer,
+} from '@mercure/rules-backend-infrastructure';
 
 @Module({
   providers: [
@@ -15,7 +25,24 @@ import { WazuhXmlRulesetAnalyzer } from '@mercure/rules-backend-infrastructure';
       useFactory: (analyzer: WazuhXmlRulesetAnalyzer) => new AnalyzeRuleset(analyzer),
       inject: [WazuhXmlRulesetAnalyzer],
     },
+    {
+      provide: RULESET_ARCHIVE_SOURCE,
+      useFactory: (config: PlatformConfig) =>
+        new FilesystemManagerArchiveSource({
+          rootPath: config.rulesManagerArchiveDir,
+          maxFiles: config.rulesArchiveMaxFiles,
+          maxEntryBytes: config.rulesArchiveMaxEntryBytes,
+          maxTotalBytes: config.rulesArchiveMaxTotalBytes,
+        }),
+      inject: [PlatformConfig],
+    },
+    {
+      provide: ImportArchivedRuleset,
+      useFactory: (source: RulesetArchiveSource, analyzeRuleset: AnalyzeRuleset) =>
+        new ImportArchivedRuleset(source, analyzeRuleset),
+      inject: [RULESET_ARCHIVE_SOURCE, AnalyzeRuleset],
+    },
   ],
-  exports: [AnalyzeRuleset],
+  exports: [AnalyzeRuleset, ImportArchivedRuleset],
 })
 export class RulesBackendModule {}
