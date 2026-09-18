e5fc096d5947649e2332c3a5a95035a44391d7ba
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-18 17:09:00 +0100

    feature:rules:export-diff-roundtrip

--- libs/modules/rules/backend/domain/src/index.ts
+++ libs/modules/rules/backend/domain/src/index.ts
@@ -22,3 +22,19 @@ export type {
   ValidationIssue,
   ValidationSeverity,
 } from './lib/rules-records';
+
+export {
+  diffRulesets,
+  type RulesetDiff,
+  type RulesetDiffItem,
+  type RulesetDiffSummary,
+} from './lib/rules-diff';
+export {
+  analyzeXmlRoundtrip,
+  type CommentedRuleRecord,
+  type GroupFlowRecord,
+  type RoundtripPatchSuggestion,
+  type SourceSectionRecord,
+  type SplitXmlRecord,
+  type XmlRoundtripAnalysis,
+} from './lib/xml-roundtrip';
