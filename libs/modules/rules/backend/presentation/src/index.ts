dbba4aabbaeefd4b86beb1c6c3fd1d7de01769c4
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-18 16:04:08 +0100

    feature:rules:export-snapshot-controller

---
+++ libs/modules/rules/backend/presentation/src/index.ts
@@ -0,0 +1,13 @@
+export { RulesSnapshotsController } from './lib/rules-snapshots.controller';
+export {
+  RulesSnapshotDecoderPageDocument,
+  RulesSnapshotDecodersQueryDto,
+  RulesSnapshotDocument,
+  RulesSnapshotIssuePageDocument,
+  RulesSnapshotIssuesQueryDto,
+  RulesSnapshotListQueryDto,
+  RulesSnapshotPageDocument,
+  RulesSnapshotParamsDto,
+  RulesSnapshotRulePageDocument,
+  RulesSnapshotRulesQueryDto,
+} from './lib/rules-snapshots.dto';
