f662228f24edbad8af161eb3f330c01d412273a5
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-18 17:11:50 +0100

    feature:rules:export-snapshot-analysis-source

--- libs/modules/rules/backend/infrastructure/src/index.ts
+++ libs/modules/rules/backend/infrastructure/src/index.ts
@@ -5,3 +5,4 @@ export {
 export { WazuhXmlRulesetAnalyzer } from './lib/wazuh-xml-ruleset-analyzer';
 export { PrismaRulesetSnapshotStore } from './lib/prisma-ruleset-snapshot-store';
 export { PrismaRulesetSnapshotQueryStore } from './lib/prisma-ruleset-snapshot-query-store';
+export { PrismaRulesetSnapshotAnalysisSource } from './lib/prisma-ruleset-snapshot-analysis-source';
