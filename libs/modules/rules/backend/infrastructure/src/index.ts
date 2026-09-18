9d682ed53ab338d6273fff560a799bfc880fadd3
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-18 13:37:21 +0100

    feature:rules:export-manager-archive-source

--- libs/modules/rules/backend/infrastructure/src/index.ts
+++ libs/modules/rules/backend/infrastructure/src/index.ts
@@ -1,1 +1,5 @@
+export {
+  FilesystemManagerArchiveSource,
+  type ManagerArchiveSourceOptions,
+} from './lib/filesystem-manager-archive-source';
 export { WazuhXmlRulesetAnalyzer } from './lib/wazuh-xml-ruleset-analyzer';
