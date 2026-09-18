a834be3420f579603e9775b5da4356a66b0534e9
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-18 14:19:23 +0100

    feature:rules:wire-snapshot-persistence

--- libs/modules/rules/backend/feature/src/lib/rules-backend.module.ts
+++ libs/modules/rules/backend/feature/src/lib/rules-backend.module.ts
@@ -2,18 +2,27 @@ import { Module } from '@nestjs/common';
 
 import { PlatformConfig } from '@mercure/platform-backend-config';
 import {
+  PlatformDatabaseModule,
+  PrismaService,
+} from '@mercure/platform-backend-database';
+import {
   AnalyzeRuleset,
   ImportArchivedRuleset,
+  PersistImportedRuleset,
   RULESET_ANALYZER,
   RULESET_ARCHIVE_SOURCE,
+  RULESET_SNAPSHOT_STORE,
   type RulesetArchiveSource,
+  type RulesetSnapshotStore,
 } from '@mercure/rules-backend-application';
 import {
   FilesystemManagerArchiveSource,
+  PrismaRulesetSnapshotStore,
   WazuhXmlRulesetAnalyzer,
 } from '@mercure/rules-backend-infrastructure';
 
 @Module({
+  imports: [PlatformDatabaseModule],
   providers: [
     WazuhXmlRulesetAnalyzer,
     {
@@ -42,7 +51,22 @@ import {
         new ImportArchivedRuleset(source, analyzeRuleset),
       inject: [RULESET_ARCHIVE_SOURCE, AnalyzeRuleset],
     },
+    {
+      provide: PrismaRulesetSnapshotStore,
+      useFactory: (database: PrismaService) => new PrismaRulesetSnapshotStore(database),
+      inject: [PrismaService],
+    },
+    {
+      provide: RULESET_SNAPSHOT_STORE,
+      useExisting: PrismaRulesetSnapshotStore,
+    },
+    {
+      provide: PersistImportedRuleset,
+      useFactory: (importer: ImportArchivedRuleset, store: RulesetSnapshotStore) =>
+        new PersistImportedRuleset(importer, store),
+      inject: [ImportArchivedRuleset, RULESET_SNAPSHOT_STORE],
+    },
   ],
-  exports: [AnalyzeRuleset, ImportArchivedRuleset],
+  exports: [AnalyzeRuleset, ImportArchivedRuleset, PersistImportedRuleset],
 })
 export class RulesBackendModule {}
