838eff2a65d0e1d33882c923c561942bb129fa18
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-19 22:27:58 +0100

    feature:rules:export-intelligence-presentation

--- libs/modules/rules/backend/presentation/src/index.ts
+++ libs/modules/rules/backend/presentation/src/index.ts
@@ -11,3 +11,25 @@ export {
   RulesSnapshotRulePageDocument,
   RulesSnapshotRulesQueryDto,
 } from './lib/rules-snapshots.dto';
+
+export {
+  RulesIntelligenceController,
+  RulesUseCasesController,
+} from './lib/rules-intelligence.controller';
+export {
+  RulesFieldIntelligenceDocument,
+  RulesFieldIntelligenceQueryDto,
+  RulesGraphDocument,
+  RulesGraphQueryDto,
+  RulesIntelligenceSnapshotParamsDto,
+  RulesQualityDocument,
+  RulesQualityQueryDto,
+  RulesRoundtripDocument,
+  RulesRoundtripQueryDto,
+  RulesSnapshotCompareDocument,
+  RulesSnapshotCompareQueryDto,
+  RulesUseCaseDocument,
+  RulesUseCaseListQueryDto,
+  RulesUseCasePageDocument,
+  RulesUseCaseParamsDto,
+} from './lib/rules-intelligence.dto';
