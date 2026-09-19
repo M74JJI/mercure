c241a3a178cee79b9518d39fa06bbfb936781810
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-19 22:38:00 +0100

    feature:rules:export-comparison-feature

--- libs/modules/rules/frontend/feature/src/index.ts
+++ libs/modules/rules/frontend/feature/src/index.ts
@@ -2,3 +2,8 @@ export { RulesOverviewFeature } from './lib/rules-overview-feature';
 export { RulesSnapshotFeature, type RulesSnapshotFeatureProps } from './lib/rules-snapshot-feature';
 
 export { RulesUseCaseCatalogFeature } from './lib/rules-use-case-catalog-feature';
+
+export {
+  RulesComparisonFeature,
+  type RulesComparisonFeatureProps,
+} from './lib/rules-comparison-feature';
