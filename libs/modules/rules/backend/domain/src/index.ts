8a86564b37c5ed6d2f3ee310228360a156ca45fa
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-19 18:40:26 +0100

    feature:rules:export-intelligence-domain

--- libs/modules/rules/backend/domain/src/index.ts
+++ libs/modules/rules/backend/domain/src/index.ts
@@ -38,3 +38,33 @@ export {
   type SplitXmlRecord,
   type XmlRoundtripAnalysis,
 } from './lib/xml-roundtrip';
+
+export {
+  buildFieldIntelligence,
+  type FieldAliasHint,
+  type FieldCriticality,
+  type FieldDecoderReference,
+  type FieldDictionaryEntry,
+  type FieldHealth,
+  type FieldIntelligenceSummary,
+  type FieldLineageRow,
+  type FieldRuleReference,
+} from './lib/field-intelligence';
+export {
+  buildQualitySummary,
+  type QualityDimension,
+  type QualityGrade,
+  type QualitySummary,
+  type RuleQualityScore,
+  type UseCaseQualityScore,
+} from './lib/rule-quality';
+export {
+  buildRulesGraph,
+  type RulesGraphData,
+  type RulesGraphEdge,
+  type RulesGraphEdgeType,
+  type RulesGraphFilters,
+  type RulesGraphMode,
+  type RulesGraphNode,
+  type RulesGraphNodeType,
+} from './lib/rules-graph';
