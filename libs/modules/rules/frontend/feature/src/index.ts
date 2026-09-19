8b9bbfb4fa7b7db78621ea7aae3ff7e1f21f761d
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-19 23:25:08 +0100

    feature:rules:export-overview-query-props

--- libs/modules/rules/frontend/feature/src/index.ts
+++ libs/modules/rules/frontend/feature/src/index.ts
@@ -1,4 +1,7 @@
-export { RulesOverviewFeature } from './lib/rules-overview-feature';
+export {
+  RulesOverviewFeature,
+  type RulesOverviewFeatureProps,
+} from './lib/rules-overview-feature';
 export { RulesSnapshotFeature, type RulesSnapshotFeatureProps } from './lib/rules-snapshot-feature';
 
 export {
