adceac0090f525aae55237af1751d0923e64b6a4
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-19 20:37:53 +0100

    feature:identity:export-http-guards

--- libs/platform/backend/presentation/src/index.ts
+++ libs/platform/backend/presentation/src/index.ts
@@ -1,3 +1,6 @@
+export { AuthenticationGuard } from './lib/authentication.guard';
+export { AuthorizationGuard } from './lib/authorization.guard';
+export type { MercureAuthenticatedRequest } from './lib/authenticated-request';
 export { HealthController } from './lib/health.controller';
 export { ProblemDetailsFilter } from './lib/problem-details.filter';
 export { StrictZodValidationPipe } from './lib/strict-zod-validation.pipe';
