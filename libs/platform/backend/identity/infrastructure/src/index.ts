19ae09fd8fe85b9e6db499c0241f5936605d6e2c
M74JJI <52024672+M74JJI@users.noreply.github.com>
2026-09-19 20:36:05 +0100

    feature:identity:export-access-token-verifier

---
+++ libs/platform/backend/identity/infrastructure/src/index.ts
@@ -0,0 +1,7 @@
+export {
+  ACCESS_TOKEN_VERIFIER,
+  InvalidAccessTokenError,
+  KeycloakAccessTokenVerifier,
+  type AccessTokenVerifier,
+  type KeycloakAccessTokenVerifierOptions,
+} from './lib/keycloak-access-token-verifier';
